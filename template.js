87c76388a5cf5dde7b5ff9db7bf22268c0c3792c
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-26 10:16:33 +0600

    template update

---
+++ template.js
@@ -0,0 +1,19 @@
+const first = "Jaan";
+const last = "pakhi";
+
+const greet = "Potas potas";
+
+const name = first + " " + last + " " + greet;
+console.log(name);
+const a = 10;
+const b = 20;
+const result = "The sum of " + a + " and " + b + " is " + (a + b);
+console.log(result);
+
+const math = `The sum of ${a} and ${b} is ${a + b}`;
+console.log(math);
+
+const challenge = `John Cena
+cena naki ochena
+mair dimu prochur`;
+console.log(challenge);
